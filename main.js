b3586fa7ad925890b8b8225f5d94d2c6b112f30b
Richard Westenra <rjwestenra@gmail.com>
2020-07-13 15:13:44 +0100

    Refactor JS a bit

--- main.js
+++ main.js
@@ -7,22 +7,25 @@
   function handleUpload() {
     if (this.files && this.files.length) {
       var file = this.files[0];
-      filetype = file.type;
-      filename = file.name;
-
-      if (/^image\/\w+$/.test(filetype)) {
+      if (/^image\/\w+$/.test(file.type)) {
+        processImage(file);
         upload.value = '';
-        h1.textContent = 'Analyzing image…';
-        setTimeout(function() {
-          var blob = URL.createObjectURL(file);
-          document.body.style.backgroundImage = 'url(' + blob + ')';
-          main.classList.add('stitch');
-          h1.textContent = 'Yes, it\'s cake';
-        }, 3000);
       } else {
         h1.textContent = 'Please choose an image file.';
       }
     }
+  }
+
+  function processImage(file) {
+    h1.textContent = 'Analyzing image…';
+    // complex machine learning neural net goes here
+    setTimeout(function() {
+      var blob = URL.createObjectURL(file);
+      document.body.style.backgroundImage = 'url(' + blob + ')';
+      main.classList.add('stitch');
+      // oh no, you caught me
+      h1.textContent = 'Yes, it\'s cake';
+    }, 3000);
   }
 
   if (URL) {
