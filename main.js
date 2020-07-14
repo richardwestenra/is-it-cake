39ffe30bc211a793282ce84e904326f327128a50
Richard Westenra <rjwestenra@gmail.com>
2020-07-14 11:28:44 +0100

    Improve machine-learning algorithm

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
   var main = document.querySelector('main');
   var h1 = document.querySelector('h1');
 
+  /**
+   * Upload the file and check if it's an image
+   */
   function handleUpload() {
     if (this.files && this.files.length) {
       var file = this.files[0];
@@ -16,16 +19,30 @@
     }
   }
 
+  /**
+   * Analyze the image and update the UI
+   * @param {object} file Uploaded image file
+   */
   function processImage(file) {
     h1.textContent = 'Analyzing image…';
-    // complex machine learning neural net goes here
     setTimeout(function() {
       var blob = URL.createObjectURL(file);
       document.body.style.backgroundImage = 'url(' + blob + ')';
       main.classList.add('stitch');
-      // oh no, you caught me
-      h1.textContent = 'Yes, it\'s cake';
+      if (isItCake()) {
+        h1.textContent = 'Yes, it\'s cake';
+      } else {
+        h1.textContent = 'No, it\'s not cake';
+      }
     }, 3000);
+  }
+
+  /**
+   * complex machine learning neural net goes here
+  */
+  function isItCake() {
+    // oh no, you caught me
+    return true;
   }
 
   if (URL) {
@@ -33,7 +50,11 @@
   } else {
     upload.addEventListener('click', function(e){
       e.preventDefault();
-      alert('The features required to make this work aren\'t supported in your browser :(');
+      h1.textContent = 'This won\'t work in your browser, sorry!';
+      var p = document.createElement('p');
+      p.textContent = 'But I\'m pretty sure that it\'s cake';
+      h1.parentElement.insertBefore(p, null);
+      upload.parentElement.remove();
     });
   }
 })();
